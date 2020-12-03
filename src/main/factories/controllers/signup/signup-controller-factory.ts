fbc9d8108ceea8ef66f5cf0c3b79d01ff05cdec2
Pedro Henrique Bergental <phbergental@brazpine.com>
2020-12-03 03:54:39 -0300

    refactor: separate factories in folder

---
+++ src/main/factories/controllers/signup/signup-controller-factory.ts
@@ -0,0 +1,11 @@
+import { makeDbAddAccount } from './../../usecases/add-account/db-add-account-factory'
+import { Controller } from '../../../../presentation/protocols'
+import { SignUpController } from '../../../../presentation/controller/signup/signup-controller'
+import { makeSignUpValidation } from './signup-validation-factory'
+import { makeDbAuthentication } from '../../usecases/authentication/db-authentication-factory'
+import { makeLogControllerDecorator } from '../../decorators/log-controller-decorator-factory'
+
+export const makeSignUpController = (): Controller => {
+  const controller = new SignUpController(makeDbAddAccount(), makeSignUpValidation(), makeDbAuthentication())
+  return makeLogControllerDecorator(controller)
+}
